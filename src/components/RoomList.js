4b407c02076b9eb292446f2742d3b25d65974738
Paul Lime <paullime@gmail.com>
2018-04-18 19:41:30 -0400

    Checkpoint 3: create rooms

--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -4,9 +4,9 @@ class RoomList extends Component {
   constructor(props) {
     super(props);
 
-
     this.state = {
-      rooms: []
+      rooms: [],
+      newRoomName: ''
     };
 
     this.roomsRef = this.props.firebase.database().ref('rooms');
@@ -14,11 +14,24 @@ class RoomList extends Component {
   }
 
   componentDidMount() {
-    this.roomsRef.on('child_added', snapshot => {
+    this.roomsRef.on('child_added', snapshot => {      
       const room = snapshot.val();
       room.key = snapshot.key;
       this.setState({ rooms: this.state.rooms.concat( room ) });      
     });
+  }
+
+  handleChange(e) {
+    this.setState({ newRoomName: e.target.value })    
+  }
+
+
+  createRoom(e) {
+    if (!this.state.newRoomName) { return }
+    e.preventDefault();
+    const newRoom = this.state.newRoomName;
+    this.roomsRef.push({ name: newRoom });
+    this.setState({ newRoomName: '' });
   }
 
   render() { 
@@ -30,12 +43,14 @@ class RoomList extends Component {
               {room.name}
               </div>)
           }
-        RoomList stuff
+        <form onSubmit={ (e) => this.createRoom(e) }>
+          <input type="text" value={ this.state.newRoomName } onChange={ (e) => this.handleChange(e) } />
+          <input type="submit" />
+        </form>
       </section>
+
     );
   }
-
-  
 }
-
+  
 export default RoomList;
